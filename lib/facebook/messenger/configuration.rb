53b770046c76cfb1e60a708c6a572dd78036190b
Pierre Alvan <pierre.alvan@gmail.com>
2016-11-12 23:24:25 -0800

    add config options

--- lib/facebook/messenger/configuration.rb
+++ lib/facebook/messenger/configuration.rb
@@ -5,6 +5,9 @@ module Facebook
     # This module holds the configuration.
     class Configuration
       attr_accessor :provider
+      attr_accessor :access_token
+      attr_accessor :app_secret
+      attr_accessor :verify_token
     end
   end
 end
